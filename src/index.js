6b58de6f9f008bacda7cd79d12f90be4204278a3
Akhil Gupta <akhilgupta01@gmail.com>
2019-10-08 20:35:44 +0530

    added security

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 import LoginComponent from "./components/loginComponent";
+import b2cauth from "react-azure-adb2c";
 
-ReactDOM.render(<LoginComponent />, document.getElementById("root"));
+b2cauth.initialize({
+  instance: "https://login.microsoftonline.com/tfp/",
+  tenant: "dbgreenworld.onmicrosoft.com",
+  signInPolicy: "B2C_1_signup",
+  applicationId: "77d19cac-e013-4cf4-9e33-64a0ab29fbbe",
+  cacheLocation: "sessionStorage",
+  scopes: ["https://dbgreenworld.onmicrosoft.com/api/user_impersonation"],
+  redirectUri: "http://localhost:3000",
+  postLogoutRedirectUri: window.location.origin
+});
+
+b2cauth.run(() => {
+  ReactDOM.render(<LoginComponent />, document.getElementById("root"));
+  serviceWorker.unregister();
+});
