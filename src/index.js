5aabd8d3b380ee63e085dfccf716ddf523da50b5
Akhil Gupta <akhilgupta01@gmail.com>
2019-10-08 23:35:29 +0530

    removed security

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import LoginComponent from "./components/loginComponent";
-import b2cauth from "react-azure-adb2c";
-
-b2cauth.initialize({
-  instance: "https://login.microsoftonline.com/tfp/",
-  tenant: "dbgreenworld.onmicrosoft.com",
-  signInPolicy: "B2C_1_signup",
-  applicationId: "77d19cac-e013-4cf4-9e33-64a0ab29fbbe",
-  cacheLocation: "sessionStorage",
-  scopes: ["https://dbgreenworld.onmicrosoft.com/api/user_impersonation"],
-  redirectUri: "https://gw-web-app.azurewebsites.net/",
-  postLogoutRedirectUri: window.location.origin
-});
 
 ReactDOM.render(<LoginComponent />, document.getElementById("root"));
 serviceWorker.unregister();
